fd72efba538ed2f8b9a9ae21a91300221fbfc682
Daniele Rispoli <daniele.rispoli@gmail.com>
2009-01-20 00:20:07 +0100

    modified:   dsl.scm 		updated parser according to new grammar, added compile 		and eval functions 	new file:   grammar 		language grammar

--- dsl.scm
+++ dsl.scm
@@ -1,22 +1,12 @@
 ;#lang scheme
 
-;; let f x y = x + y in
-;; 	let z = 3 in
-;; 		if [(true)|true] then
-;; 			f (2 + 3 * 5) (3 + 1 - 1 * 2)
-;; 		else
-;; 			z
-;; 
-;; 
-;; /usr/lib/plt/collects/parser-tools/examples/
-
 (require parser-tools/yacc
-		 parser-tools/lex
-		 (prefix-in : parser-tools/lex-sre)
-		 srfi/1)
+         parser-tools/lex
+         (prefix-in : parser-tools/lex-sre)
+         srfi/1)
 
 (define-tokens value-tokens (NUM BOOL VAR))
-(define-empty-tokens op-tokens (= OP CP + - * / ^ EOF NEG && \|\| ~ == != LET IN IF THEN ELSE))
+(define-empty-tokens op-tokens (= OP CP + - * / ^ EOF NEG && \|\| ~ == != > < >= <= @ LAMBDA -> LET LETREC IN IF THEN ELSE))
 
 (define-lex-abbrevs
   (lower-letter (:/ "a" "z"))
@@ -25,66 +15,78 @@
 
 (define dsll
   (lexer
-	((eof) 'EOF)
-	((:or #\newline #\return #\tab #\space #\vtab) (dsll input-port))
-	((:or "=" "+" "-" "*" "/" "^" "&&" "||" "~" "==" "!=") (string->symbol lexeme))
-	("(" 'OP)
-	(")" 'CP)
-	("true" (token-BOOL #t))
-	("false" (token-BOOL #f))
-	("let" 'LET)
-	("in" 'IN)
-	("if" 'IF)
-	("then" 'THEN)
-	("else" 'ELSE)
-	((:+ digit) (token-NUM (string->number lexeme)))
-	((:+ (:or lower-letter upper-letter "_")) (token-VAR (string->symbol lexeme)))))
+    ((eof) 'EOF)
+    ((:or #\newline #\return #\tab #\space #\vtab) (dsll input-port))
+    ((:or "=" "+" "-" "*" "/" "^" "&&" "||" "~" "==" "!=" ">" "<" ">=" "<=" "@" "->") (string->symbol lexeme))
+    ("(" 'OP)
+    (")" 'CP)
+    ("true" (token-BOOL #t))
+    ("false" (token-BOOL #f))
+    ("lambda" 'LAMBDA)
+    ("let" 'LET)
+    ("letrec" 'LETREC)
+    ("in" 'IN)
+    ("if" 'IF)
+    ("then" 'THEN)
+    ("else" 'ELSE)
+    ((:+ digit) (token-NUM (string->number lexeme)))
+    ((:: (:+ (:or lower-letter upper-letter)) (:* (:or lower-letter upper-letter "_" digit))) (token-VAR (string->symbol lexeme)))))
 
 (define dslp
   (parser
-	(start exp)
-	(end EOF)
-	(tokens value-tokens op-tokens)
-	(error (lambda (tok-ok? tok-name tok-value)
-			 (display "Error:")	; format ...
-			 (display tok-name)
-			 (display tok-value)))
-	;(suppress)
+    (start exp)
+    (end EOF)
+    (tokens value-tokens op-tokens)
+    (error (lambda (tok-ok? tok-name tok-value)
+             (printf "Error: ~a ~a ~a~%" tok-ok? tok-name tok-value)))
+    ;(suppress)
 
-	(precs (right = == !=)
-		   (left - +)
-		   (left * /)
-		   (left NEG)
-		   (left ^)
-		   (left \|\|)
-		   (left &&)
-		   (left ~))
+    (precs (right = == != > < >= <=)
+           (left - +)
+           (left * /)
+           (left NEG)
+           (left ^)
+           (left \|\|)
+           (left &&)
+           (left ~))
 
-	(grammar
-	  (ar-exp ((NUM) $1)
-			  ((VAR) $1)
-			  ((ar-exp + ar-exp) `(+ ,$1 ,$3))
-			  ((ar-exp - ar-exp) `(- ,$1 ,$3))
-			  ((ar-exp * ar-exp) `(* ,$1 ,$3))
-			  ((ar-exp / ar-exp) `(/ ,$1 ,$3))
-			  ((- ar-exp) (prec NEG) `(- ,$2))
-			  ((ar-exp ^ ar-exp) `(expt ,$1 ,$3))
-			  ((OP ar-exp CP) $2))
-	  (bl-exp ((BOOL) $1)
-			  ((VAR) $1)
-			  ((bl-exp && bl-exp) `(and ,$1 ,$3))
-			  ((bl-exp \|\| bl-exp) `(or ,$1 ,$3))
-			  ((~ bl-exp) (prec ~) `(not ,$2))
-			  ((ar-exp == ar-exp) `(= ,$1 ,$3))
-			  ((ar-exp != ar-exp) `(not (= ,$1 ,$3)))
-			  ((OP bl-exp CP) $2))
-	  (exp-list (() '())
-				((exp-list exp) (cons $2 $1)))
-	  (var-list (() '())
-				((var-list VAR) (cons $2 $1)))
-	  (exp ((ar-exp) $1)
-		   ((bl-exp) $1)
-		   ((OP VAR exp-list CP) `(,$2 ,@(reverse $3)))
-		   ((IF bl-exp THEN exp ELSE exp) `(if ,$2 ,$4 ,$6))
-		   ((LET VAR var-list = exp IN exp) `(let ((,$2 ,(foldl (lambda (e p) `(lambda (,e) ,p)) $5 $3))) ,$7)))
-	  )))
+    (grammar
+      (prefix-op ((-) '-) ((~) 'not))
+      (infix-op ((+) '+) ((-) '-) ((*) '*) ((/) '/) ((^) 'expt) ((&&) 'and) ((\|\|) 'or) ((==) '=) ((!=) '!=) ((>) '>) ((<) '<) ((>=) '>=) ((<=) '<=))
+      (var-list (() '())
+                ((var-list VAR) (cons $2 $1)))
+      (exp-list (() '())
+                ((exp-list exp) (cons $2 $1)))
+      (exp ((BOOL) $1)
+           ((NUM) $1)
+           ((VAR) $1)
+           ((OP exp CP) $2)
+           ((prefix-op exp) (prec NEG) (list $1 $2))
+           ((exp infix-op exp) (list $2 $1 $3))
+           ((IF exp THEN exp ELSE exp) `(if ,$2 ,$4 ,$6))
+           ((LET VAR var-list = exp) `(define ,$2 ,(if (> (length $3) 0) `(lambda (,@(reverse $3)) ,$5) $5)))
+           ((LET VAR var-list = exp IN exp) `(let ((,$2 ,(if (> (length $3) 0) `(lambda (,@(reverse $3)) ,$5) $5))) ,$7)) ; uncurried
+           ((LETREC VAR var-list = exp IN exp) `(letrec ((,$2 ,(if (> (length $3) 0) `(lambda (,@(reverse $3)) ,$5) $5))) ,$7)) ; uncurried
+           ((LAMBDA var-list -> exp) `(lambda (,@(reverse $2)) ,$4))
+           ((@ OP exp exp-list CP) `(,$3 ,@(reverse $4)))))))
+
+(define !=
+  (lambda (x y)
+    (not (= x y))))
+
+(define c
+  (lambda (s)
+    (let ((ois (open-input-string s)))
+      (dslp (lambda ()
+              (dsll ois))))))
+
+(define e
+  (lambda (s)
+    (eval (c s))))
+
+; (c "5 + if true then 2 else 3")
+; (c "let f x y = x + y in let z = 3 in if false then @(f (2+3*5) (3+1-1*2)) else z")
+; (c "let phi x = (x+1)*(x-2)*((x-4)/2)")
+; (c "let x = 3")
+; (c "lambda n m -> @(phi n) + @(phi m)")
+; (c "@((lambda x y -> x + y) 2 3)")
