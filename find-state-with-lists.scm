cb726a4327c00ad0160a2e5103febd1dd81a1eff
Daniele Rispoli <daniele.rispoli@gmail.com>
2009-01-23 21:49:01 +0100

    modified:   find-state-with-lists.scm 		added state-consistent? function to check if the whole 		given state is consistent and expanded tabs to spaces

--- find-state-with-lists.scm
+++ find-state-with-lists.scm
@@ -4,16 +4,25 @@
 
 (define already-in?
   (lambda (fi-sn sn)
-	(cond
-	  ((null? sn) #f)
-	  ((member fi-sn sn) (list-ref fi-sn 2)) ; _ != #f ==> #t 
-	  (else (already-in? fi-sn (cdr sn))))))
+    (cond
+      ((null? sn) #f)
+      ((member fi-sn sn) (list-ref fi-sn 2)) ; _ != #f ==> #t 
+      (else (already-in? fi-sn (cdr sn))))))
 
 (define consistent?
   (lambda (fi-sn sn)
-	(let ((n (list-ref fi-sn 1)) (m (list-ref fi-sn 2)))
-	  (and (not (already-in? fi-sn sn)) ((car fi-sn) n m)))))
+    (let ((n (list-ref fi-sn 1)) (m (list-ref fi-sn 2)))
+      (and (not (already-in? fi-sn sn)) ((car fi-sn) n m)))))
 
 (define incr-know
   (lambda (fi-sn sn)
-	(append sn (list fi-sn))))
+    (append sn (list fi-sn))))
+
+(define state-consistent?
+  (lambda (s)
+    (foldl (lambda (a b) (and a b))
+           #t
+           (map (lambda (e)
+                  (let ((P (list-ref e 0)) (x (list-ref e 1)) (y (list-ref e 2)))
+                    (P x y)))
+                s))))
