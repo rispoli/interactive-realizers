479a1c3b6367471c96b2aa9686ce180824350f17
Daniele Rispoli <daniele.rispoli@gmail.com>
2009-01-27 17:41:17 +0100

    modified:   coquand.scm 		- tabs, + spaces

--- coquand.scm
+++ coquand.scm
@@ -4,7 +4,7 @@
 
 (define phi
   (lambda (x)
-	(* (+ x 1) (- x 2) (/ (- x 4) 2))))
+    (* (+ x 1) (- x 2) (/ (- x 4) 2))))
 
 (define a 1)
 
@@ -17,25 +17,20 @@
 
 (define m
   (lambda (s)
-	(let ((n0 1))
-	  (mu s n0))))
+    (let ((n0 1))
+      (mu s n0))))
 
-;; Utilizzando al posto di un nome generico il predicato (o un qualche
-;; "puntatore" ad esso, come nell'articolo) l'implementazione di 
-;; consistent? risulta piu' facilmente generalizzabile: se ci fosse solo un nome
-;; e non un modo per risalire da quest'ultimo al predicato bisognerebbe
-;; passargliero in qualche altra maniera.
 (define F
   (lambda (_)
-	(list (lambda (s)
-			(list (lambda (n m)
-					(> (phi n) (phi m)))
-				  (m s)
-				  (+ (m s) a))))))
+    (list (lambda (s)
+            (list (lambda (n m)
+                    (> (phi n) (phi m)))
+                  (m s)
+                  (+ (m s) a))))))
 
 (define sat-phi?
   (lambda (x)
-	(<= (phi x) (phi (+ x a)))))
+    (<= (phi x) (phi (+ x a)))))
 
 (define final-state (find-state F '()))
 
