f8875e9e0030cb409a2fa8413d20a582bfdb0be3
Daniele Rispoli <daniele.rispoli@gmail.com>
2009-06-19 19:27:21 +0200

    new file:   coquand.dsl.ml 		Coquand's example ported to OCaml

---
+++ coquand.dsl.ml
@@ -0,0 +1,13 @@
+let phi x = (x + 1) * (x - 2) * ((x - 4) / 2);;
+
+let a = 1;;
+
+(* mu <state> <initial_value> *)
+let m s = mu s 1;;
+
+let p n m = phi n > phi m;;
+
+let fam s =
+    [
+        fun s -> ( p, m s, (m s) + a )
+    ];;
